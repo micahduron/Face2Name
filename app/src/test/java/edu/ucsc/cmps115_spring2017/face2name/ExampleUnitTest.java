c8bad7b84c84b6bd61165b3b3b2f3c250875aed1
Micah Duron <micah@duron.me>
2017-04-14 15:20:23 -0700

    Setup initial Android environment
    This is our starting point for our app.

---
+++ app/src/test/java/edu/ucsc/cmps115_spring2017/face2name/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.ucsc.cmps115_spring2017.face2name;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
