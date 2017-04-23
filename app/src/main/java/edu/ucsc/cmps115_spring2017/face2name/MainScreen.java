023485eea92ddbe1265dc22051e45684eb5568dd
Micah Duron <micah@duron.me>
2017-04-22 21:20:25 -0700

    Implement a basic main activity
    This is a very bare bones implementation of the main app window.
    We will be building on this in later commits.

--- app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/MainScreen.java
+++ app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/MainScreen.java
@@ -1,29 +1,66 @@
 package edu.ucsc.cmps115_spring2017.face2name;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainScreen extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+public class MainScreen extends AppCompatActivity implements CameraPreview.PreviewCallbacks {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main_screen);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+    public void onStart() {
+        super.onStart();
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mCameraPreview = (CameraPreview) findViewById(R.id.camera_preview);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mCameraPreview.init(CameraPreview.BACK_CAMERA, this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mCameraPreview.release();
+    }
+
+    @Override
+    public void onCameraStart() {
+
+    }
+
+    @Override
+    public void onCameraRelease() {
+
+    }
+
+    @Override
+    public void onCameraReady(int width, int height) {
+        mCameraPreview.setBitmap(Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888));
+
+        mCameraPreview.startPreview();
+    }
+
+    @Override
+    public void onPreviewResize(int width, int height) {
+
+    }
+
+    @Override
+    public void onPreviewFrame(Bitmap bitmap) {
+
+    }
+
+    private CameraPreview mCameraPreview;
 }
