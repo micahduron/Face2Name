c38584c9349f88dd6436eb290e45b799ae55d9fa
Miao-Long <myau.christopher@gmail.com>
2017-04-25 18:44:53 -0700

    Add Layer Class

--- app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/Layer/LayerView.java
+++ app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/Layer/LayerView.java
@@ -4,5 +4,36 @@ package edu.ucsc.cmps115_spring2017.face2name.Layer;
  * Created by Chris Myau on 4/25/2017.
  */
 
-public class LayerView {
+import android.util.AttributeSet;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.View;
+
+public final class LayerView extends View
+{
+    private Rect rectangle;
+    private Paint paint;
+
+    public LayerView(Context context) {
+        super(context);
+        int x = 50;
+        int y = 50;
+        int sideLength = 200;
+
+        // create a rectangle that we'll draw later
+        rectangle = new Rect(x, y, sideLength, sideLength);
+
+        // create the Paint and set its color
+        paint = new Paint();
+        paint.setColor(Color.GRAY);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLUE);
+        canvas.drawRect(rectangle, paint);
+    }
 }
