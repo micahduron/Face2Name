cffd840f83f579be09f3ce6a625995423b547acd
Micah Duron <mduron@ucsc.edu>
2017-04-27 12:59:21 -0700

    Working commit of FaceDetectionCapability
    This class controls the face detection feature built into devices' cameras.

---
+++ app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/Camera/FaceDetectionCapability.java
@@ -0,0 +1,80 @@
+package edu.ucsc.cmps115_spring2017.face2name.Camera;
+
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.hardware.Camera;
+
+/**
+ * Created by micah on 4/21/17.
+ */
+
+public final class FaceDetectionCapability extends CameraCapability implements Camera.FaceDetectionListener {
+    public FaceDetectionCapability(FaceDetectionListener listener) {
+        mListener = listener;
+    }
+
+    void startFaceDetection() {
+        if (mCameraInst == null) {
+            throw new RuntimeException("Cannot call startFaceDetection in an uninitialized state.");
+        }
+        mCameraInst.getCamera().startFaceDetection();
+    }
+
+    void stopFaceDetection() {
+        if (mCameraInst == null) {
+            throw new RuntimeException("Cannot call stopFaceDetection in an uninitialized state.");
+        }
+        mCameraInst.getCamera().stopFaceDetection();
+    }
+
+    public interface FaceDetectionListener {
+        void onFaceDetection(Face[] faces);
+    }
+
+    public class Face {
+        public Face(Rect rect) {
+            mRect = rect;
+        }
+
+        Rect getRect() {
+            return mRect;
+        }
+
+        private Rect mRect;
+    }
+
+    @Override
+    public void onAttach(CameraInstance cameraInst) {
+        mCameraInst = cameraInst;
+        mCameraInst.getCamera().setFaceDetectionListener(this);
+    }
+
+    @Override
+    public Bitmap onPreFrame(CameraInstance cameraInst) {
+        return null;
+    }
+
+    @Override
+    public void onFrame(Bitmap bitmap, CameraInstance cameraInst) {
+
+    }
+
+    @Override
+    public void onRelease(CameraInstance cameraInst) {
+        mCameraInst.getCamera().stopFaceDetection();
+        mCameraInst = null;
+    }
+
+    @Override
+    public void onFaceDetection(Camera.Face[] faces, Camera camera) {
+        Face[] faceObjs = new Face[faces.length];
+
+        for (int i = 0; i < faces.length; ++i) {
+            faceObjs[i] = new Face(faces[i].rect);
+        }
+        mListener.onFaceDetection(faceObjs);
+    }
+
+    private CameraInstance mCameraInst;
+    private FaceDetectionListener mListener;
+}
