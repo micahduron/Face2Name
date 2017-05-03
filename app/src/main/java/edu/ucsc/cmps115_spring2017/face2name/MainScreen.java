a06a41367ed3da83a7268ea40fb8eae062e0c19a
Micah Duron <micah@duron.me>
2017-05-03 12:44:43 -0700

    Add code to demonstrate face detection capability
    Bounding rectangles are drawn around people's faces. One thing to note
    is that a camera's built-in face detection system can only detect faces
    up to a certain maximum (five on my phone). Also, the camera's focus
    affects its ability to detect faces.

--- app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/MainScreen.java
+++ app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/MainScreen.java
@@ -1,21 +1,26 @@
 package edu.ucsc.cmps115_spring2017.face2name;
 
+import android.graphics.Matrix;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
-import android.app.AlertDialog;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import edu.ucsc.cmps115_spring2017.face2name.Camera.CameraPreview;
+import edu.ucsc.cmps115_spring2017.face2name.Camera.FaceDetectionCapability;
+import edu.ucsc.cmps115_spring2017.face2name.Camera.FaceDetectionCapability.Face;
 import edu.ucsc.cmps115_spring2017.face2name.Camera.OrientationCapability;
 import edu.ucsc.cmps115_spring2017.face2name.Layer.LayerView;
 
-public class MainScreen extends AppCompatActivity implements CameraPreview.PreviewCallbacks{
-
+public class MainScreen
+       extends AppCompatActivity
+       implements CameraPreview.PreviewCallbacks,
+                  FaceDetectionCapability.FaceDetectionListener
+{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +30,18 @@ public class MainScreen extends AppCompatActivity implements CameraPreview.Previ
     @Override
     public void onStart() {
         super.onStart();
+
+        mOrientation = new OrientationCapability(getWindowManager().getDefaultDisplay());
+        mFaceDetector = new FaceDetectionCapability(this);
         
         mCameraPreview = (CameraPreview) findViewById(R.id.camera_preview);
-        mCameraPreview.setCapabilities(new OrientationCapability(getWindowManager().getDefaultDisplay()));
+        mCameraPreview.setCapabilities(mOrientation, mFaceDetector);
+
+        mFaceTransform = FaceDetectionCapability.getFaceTransform(
+                mOrientation.getCameraAngle(),
+                mLayerView.getWidth(),
+                mLayerView.getHeight()
+        );
         mLayerView = (LayerView) findViewById(R.id.layer_view);
         mName = (EditText)findViewById(R.id.name_text);
     }
@@ -53,7 +67,8 @@ public class MainScreen extends AppCompatActivity implements CameraPreview.Previ
 
     @Override
     public void onCameraRelease() {
-
+        mCameraPreview.stopPreview();
+        mFaceDetector.stopFaceDetection();
     }
 
     @Override
@@ -64,8 +79,26 @@ public class MainScreen extends AppCompatActivity implements CameraPreview.Previ
     @Override
     public void onPreviewReady() {
         mCameraPreview.startPreview();
+        mFaceDetector.startFaceDetection();
     }
 
+    @Override
+    public void onFaceDetection(Face[] faces) {
+        LayerView.Drawer drawer = mLayerView.getDrawer();
+        drawer.beginDrawing();
+        drawer.clearScreen();
+
+        if (faces.length > 0) {
+            RectF faceRect = new RectF();
+
+            for (final Face face : faces) {
+                mFaceTransform.mapRect(face.getRect(), faceRect);
+
+                drawer.drawBox(faceRect);
+            }
+        }
+        drawer.endDrawing();
+    }
 
     /*
     This helper function checks the input event against a few other events that might occur
@@ -120,6 +153,8 @@ public class MainScreen extends AppCompatActivity implements CameraPreview.Previ
 
     private EditText mName;
     private CameraPreview mCameraPreview;
+    private OrientationCapability mOrientation;
+    private FaceDetectionCapability mFaceDetector;
+    private Matrix mFaceTransform;
     private LayerView mLayerView;
 }
-
