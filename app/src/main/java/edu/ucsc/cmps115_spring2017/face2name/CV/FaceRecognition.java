cecacd3df55aba326f4083222e23eac54d73c8f1
evanlouie <ejlouie@ucsc.edu>
2017-06-01 00:03:36 -0700

    Fixed null object problem

--- app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/CV/FaceRecognition.java
+++ app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/CV/FaceRecognition.java
@@ -48,7 +48,7 @@ public final class FaceRecognition {
 
     public Identity addFace(Image face) {
         long faceId = UUID.randomUUID().getLeastSignificantBits();
-
+        Log.d("Face", "Face added");
         return addFace(face, faceId);
     }
 
@@ -69,6 +69,7 @@ public final class FaceRecognition {
         mIdSet.add(id);
 
         native_addToModel(faceImage.getMat().getNativeObjAddr(), id);
+        Log.d("Face", "Face added to model");
     }
 
     private native void native_addToModel(long matPtr, long id);
@@ -76,7 +77,7 @@ public final class FaceRecognition {
     public RecognitionResult identify(Image faceImage) {
         Image normalizedFace = normalizeFace(faceImage);
 
-        //Log.d("Face", "Face identified");
+        Log.d("Face", "Face identified");
 
         if (normalizedFace == null) {
             return new RecognitionResult(RECOG_FAILED);
