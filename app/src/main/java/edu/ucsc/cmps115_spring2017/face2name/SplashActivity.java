9fbd7b6c6458496566714ec1022a7e5ab9e50ad4
evanlouie <ejlouie@ucsc.edu>
2017-06-03 22:25:03 -0700

    added splash class + xml

---
+++ app/src/main/java/edu/ucsc/cmps115_spring2017/face2name/SplashActivity.java
@@ -0,0 +1,37 @@
+package edu.ucsc.cmps115_spring2017.face2name;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainScreen.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
